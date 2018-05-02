e0ba8eab982273361b1a3afdfabb5340727f5c1d
FelixAlvarado <felixalvarado653@gmail.com>
2018-05-01 20:48:29 -0700

    finished rails assignment

---
+++ W3D3/demo_project/app/models/house.rb
@@ -0,0 +1,10 @@
+class House < ApplicationRecord
+  validates :address, presence: true
+
+  has_many(
+    :people,
+    class_name: 'Person',
+    foreign_key: :person_id,
+    primary_key: :id
+  )
+end
